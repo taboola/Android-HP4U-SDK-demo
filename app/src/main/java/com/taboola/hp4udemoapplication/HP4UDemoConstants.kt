c86651f6f7e00727986974d1e884334373ab95d5
Haytham Kalifa <haytham.k@taboola.com>
2022-07-03 11:01:39 +0300

    Passing URL using SharedViewModel.kt to ArticleScreenFragment.kt

--- app/src/main/java/com/taboola/hp4udemoapplication/HP4UDemoConstants.kt
+++ app/src/main/java/com/taboola/hp4udemoapplication/HP4UDemoConstants.kt
@@ -2,5 +2,5 @@ package com.taboola.hp4udemoapplication
 
 object HP4UDemoConstants {
     const val DEFAULT_PUBLISHER_NAME = "sdk-tester-hp4u-demo"
-    const val DEFAULT_API_KEY = " 05380b1d71ca985df52d641e1f0336ebbb8d67f7"
+    const val DEFAULT_API_KEY = "05380b1d71ca985df52d641e1f0336ebbb8d67f7"
 }
