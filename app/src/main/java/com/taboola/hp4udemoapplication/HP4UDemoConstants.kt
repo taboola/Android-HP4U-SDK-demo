273a3c48fe7f8edc034416be9c675cdf9b6b6338
Tomer Ben-Rachel <tomer.b@taboola.com>
2022-07-10 17:14:28 +0300

    feature/MOB-551-Usage-Event-V3: Correcting constant name

--- app/src/main/java/com/taboola/hp4udemoapplication/HP4UDemoConstants.kt
+++ app/src/main/java/com/taboola/hp4udemoapplication/HP4UDemoConstants.kt
@@ -4,5 +4,5 @@ object HP4UDemoConstants {
     const val DEFAULT_PUBLISHER_NAME = "sdk-tester-hp4u-demo"
     const val DEFAULT_API_KEY = "05380b1d71ca985df52d641e1f0336ebbb8d67f7"
     const val HP4U_MOBILE_USAGE_EVENT_KEY = "HP4UMobileEvent"
-    const val HP4U_MOBILE_USAGE_EVENT_VALUE = "HP4U_ANDROID_USED"
+    const val HP4U_MOBILE_USAGE_EVENT = "HP4U_ANDROID_USED"
 }
