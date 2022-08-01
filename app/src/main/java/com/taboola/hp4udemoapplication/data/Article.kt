dc1e839fa783dd4038c556b404608446528cb7c1
slava.v <slava.v@taboola.com>
2022-08-01 15:49:28 +0300

    switching hardcoded image to be local and not fetched from remote. and fixing to articles which had broken images

--- app/src/main/java/com/taboola/hp4udemoapplication/data/Article.kt
+++ app/src/main/java/com/taboola/hp4udemoapplication/data/Article.kt
@@ -3,7 +3,7 @@ package com.taboola.hp4udemoapplication.data
 data class Article(
     var title: String,
     var content: String,
-    var imageUrl: String,
+    var imageUrl: Int,
     var url: String,
     var category: String,
     var sectionName: String
