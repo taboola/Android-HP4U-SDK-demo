f1db92622132ccdecccbbebaf8e57bd7c0331fc5
Tomer Ben-Rachel <tomer.b@taboola.com>
2022-08-15 16:50:03 +0300

    feature/MOB-1026-Cleanup: Removing class before move to module

--- app/src/main/java/com/taboola/hp4udemoapplication/HP4UDemoUsageEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.taboola.hp4udemoapplication
-
-import com.taboola.android.global_components.eventsmanager.events.TBLMobileEvent
-
-class HP4UDemoUsageEvent(eventType: String?, data: Map<String, String>) :
-    TBLMobileEvent(eventType, data) {
-}
