c92ee04237bdfc4387b1dbe3b74822bb3fda7678
Tomer Ben-Rachel <tomer.b@taboola.com>
2022-08-15 16:12:36 +0300

    feature/MOB-1026-Cleanup: Converting constructor to be the primary constructor

--- app/src/main/java/com/taboola/hp4udemoapplication/HP4UDemoUsageEvent.kt
+++ app/src/main/java/com/taboola/hp4udemoapplication/HP4UDemoUsageEvent.kt
@@ -2,6 +2,6 @@ package com.taboola.hp4udemoapplication
 
 import com.taboola.android.global_components.eventsmanager.events.TBLMobileEvent
 
-class HP4UDemoUsageEvent :TBLMobileEvent {
-    constructor(eventType: String?, data: Map<String, String>) : super(eventType, data) {}
+class HP4UDemoUsageEvent(eventType: String?, data: Map<String, String>) :
+    TBLMobileEvent(eventType, data) {
 }
