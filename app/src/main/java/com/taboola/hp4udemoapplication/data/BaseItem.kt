07e92cbcd7cc32f417da9c34776eba3ceb8caa6a
Tomer Ben-Rachel <tomer.b@taboola.com>
2022-08-15 16:09:45 +0300

    feature/MOB-1026-Cleanup: Removing files from previous module

--- app/src/main/java/com/taboola/hp4udemoapplication/data/BaseItem.kt
+++
@@ -1,9 +0,0 @@
-package com.taboola.hp4udemoapplication.data
-
- open class BaseItem(var type: Int) {
-
-    companion object {
-        internal const val HEADER_TYPE = 0
-        internal const val ARTICLE_TYPE = 1
-    }
-}
--- app/src/main/java/com/taboola/hp4udemoapplication/data/Header.kt
+++
@@ -1,3 +0,0 @@
-package com.taboola.hp4udemoapplication.data
-
-data class Header(val title: String) : BaseItem(HEADER_TYPE)
